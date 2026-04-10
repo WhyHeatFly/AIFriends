bb004bf5f8d438c9f3713f990945d775bf0ac520
WayHeatFly <103401882+WhyHeatFly@users.noreply.github.com>
2026-04-10 23:15:09 +0800

    实现首页前后端

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -5,6 +5,7 @@ from web.views.create.character.get_list import GetListCharacterView
 from web.views.create.character.get_single import GetSingleView
 from web.views.create.character.remove import RemoveCharacterView
 from web.views.create.character.update import UpdateCharacterViews
+from web.views.homepage.index import HomepageIndexView
 from web.views.index import index
 from web.views.user.account.get_user_info import GetUserInfoView
 from web.views.user.account.login import LoginView
@@ -27,6 +28,7 @@ urlpatterns = [
     path('api/create/character/update/', UpdateCharacterViews.as_view()),
     path('api/create/character/get_single/', GetSingleView.as_view()),
     path('api/create/character/get_list/', GetListCharacterView.as_view()),
+    path('api/homepage/index/', HomepageIndexView.as_view()),
     path('', index),
 
     # 兜底路由
--- backend/web/views/homepage/index.py
+++ backend/web/views/homepage/index.py
@@ -1,23 +1,14 @@
-# 返回个人主页的用户信息和角色列表，当前设计个人主页不需要登录
-from django.contrib.auth.models import User
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from web.models.character import Character
-from web.models.user import UserProfile
 
 
-class GetListCharacterView(APIView):
+class HomepageIndexView(APIView):
     def get(self, request):
         try:
             items_count = int(request.query_params.get('items_count'))
-            user_id = request.query_params.get('user_id')
-            user = User.objects.get(id=user_id)
-            user_profile = UserProfile.objects.get(user=user)
-            characters_raw = Character.objects.filter(
-                author=user_profile
-            ).order_by('-id')[items_count : items_count + 20]
+            characters_raw = Character.objects.all().order_by('-id')[items_count:items_count + 20]
             characters = []
             for character in characters_raw:
                 author = character.author
@@ -35,15 +26,9 @@ class GetListCharacterView(APIView):
                 })
             return Response({
                 'result': 'success',
-                'user_profile': {
-                    'user_id': user.id,
-                    'username': user.username,
-                    'profile': user_profile.profile,
-                    'photo': user_profile.photo.url,
-                },
-                'characters': characters,
+                'characters': characters
             })
         except:
             return Response({
-                'resule': '获取个人主页失败，请稍后再试'
+                'result': '获取个人首页异常，请稍后再试'
             })
